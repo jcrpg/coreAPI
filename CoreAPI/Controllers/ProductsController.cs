4655d0d37b03634414e1f8e524801bd19edd5eff
Jung Chen <jcchen1@hotmail.com>
2018-09-18 22:01:42 +1000

    initial checkin

---
+++ CoreAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+       static List<Product> _products = new List<Product>()
+        {
+            new Product(){ProductId= 0,ProductName="Laptop", ProductPrice="200"},
+            new Product(){ProductId= 1,ProductName="SmartPhone", ProductPrice="200"}
+        };
+
+        public IEnumerable<Product> Get() {
+            return _products;
+        }
+
+            public void Post(Product product)
+        {
+            _products.Add(product);
+        }
+    }
+}
---
+++ CoreAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductPrice { get; set; }
+    }
+}
